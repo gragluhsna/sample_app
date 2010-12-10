3545994439187beb29ab23195bc5c740b85fd3b7
agarg <anshul.garg@gmail.com>
2010-12-10 18:53:36 +0530

    Help page and title changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    return "#{base_title} | #{@title}" if !@title.nil? 
+    return base_title
+  end
 end
