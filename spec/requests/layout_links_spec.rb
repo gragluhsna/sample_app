825f840fcb8cfa9fd373f6b481b748a35554ca4f
agarg <anshul.garg@gmail.com>
2010-12-11 11:34:43 +0530

    Done with routes and signup

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -6,5 +6,25 @@ describe "LayoutLinks" do
       get '/'
       response.should have_selector('title' , :content => 'Home')
     end
+    
+    it "should get about page at '/about'" do
+      get '/about'
+      response.should have_selector('title' , :content => 'About')
+    end
+    
+    it "should get help page at '/help'" do
+      get '/help'
+      response.should have_selector('title' , :content => 'Help')
+    end
+    
+    it "should get contact page at '/contact'" do
+      get '/contact'
+      response.should have_selector('title' , :content => 'Contact')
+    end
+    
+    it "should get signup page at 'signup'" do
+      get '/signup'
+      response.should have_selector('title' , :content => 'Sign up')
+    end
   end
 end
