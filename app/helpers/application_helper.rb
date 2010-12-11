c152a8e003dd32ac044d3d1594adca3373486831
agarg <anshul.garg@gmail.com>
2010-12-11 12:04:14 +0530

    took out logo in helper, added tests for links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,8 @@ module ApplicationHelper
     return "#{base_title} | #{@title}" if !@title.nil? 
     return base_title
   end
+  
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
